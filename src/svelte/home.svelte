b10b64084ad957095e07ce2f02329154b3e38d59
Syudagye <44324659+Syudagye@users.noreply.github.com>
2021-03-04 17:42:10 +0100

    Start button fix

--- src/svelte/home.svelte
+++ src/svelte/home.svelte
@@ -1,6 +1,13 @@
 <script>
     import Header from './header.svelte'
     import Svg from './svg.svelte'
+
+    function onStartButtonClick() {
+        if (window.account.isAuthentified)
+            window.location.href = "/dashboard"
+        else
+            window.open('/discord_auth', '_blank', 'width=550,height=900,dependent=yes,floating=yes')
+    }
 </script>
 
 <Header/>
@@ -14,7 +21,7 @@
     </div>
 
     <div class="home-page__buttons">
-        <button class="home-page__start-button button">
+        <button on:click={() => onStartButtonClick()} class="home-page__start-button button">
             <span class="button__inner-text">Start</span>
         </button>
         <a class="home-page__learn-more link" href="/">Learn More</a>
